86e72c134f2fa5cdee1699881c5b85284e8533b2
saravanan2k03 <112747138+saravanan2k03@users.noreply.github.com>
2024-06-29 15:55:52 +0530

    fix the offerCards bottom

--- lib/pages/Offer/view/OfferPage.dart
+++ lib/pages/Offer/view/OfferPage.dart
@@ -13,59 +13,68 @@ class Offerpage extends StatelessWidget {
       height: MediaQuery.of(context).size.height,
       child: Stack(
         children: [
-          Column(
-            children: [
-              SizedBox(
-                height: height(context, 0.04),
-              ),
-              SizedBox(
-                // color: Colors.red,
-                height: height(context, 0.08),
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      // color: Colors.yellow,
-                      width: width(context, 0.20),
-                      child: const Text(
-                        "Offer",
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: height(context, 0.04),
+                ),
+                SizedBox(
+                  // color: Colors.red,
+                  height: height(context, 0.08),
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        // color: Colors.yellow,
+                        width: width(context, 0.20),
+                        child: const Text(
+                          "Offer",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
                       ),
-                    ),
-                    const CustomAppbar()
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: height(context, 0.05),
-              ),
-              SizedBox(
-                width: width(context, 0.80),
-                height: height(context, 0.80),
-                // decoration: BoxDecoration(
-                //   color: const Color(0xffE0DFDF),
-                //   borderRadius: BorderRadius.circular(15),
-                // ),
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4, // number of items in each row
-                    mainAxisSpacing: 8.0, // spacing between rows
-                    crossAxisSpacing: 9.0, // spacing between columns
+                      const CustomAppbar()
+                    ],
                   ),
-                  padding: const EdgeInsets.all(8.0), // padding around the grid
-                  itemCount: 08, // total number of items
-                  itemBuilder: (context, index) {
-                    return OfferCards(
-                      index: index,
-                    );
-                  },
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: height(context, 0.05),
+                ),
+                SizedBox(
+                  width: width(context, 0.80),
+                  height: height(context, 0.80),
+                  // decoration: BoxDecoration(
+                  //   color: const Color(0xffE0DFDF),
+                  //   borderRadius: BorderRadius.circular(15),
+                  // ),
+                  child: GridView.builder(
+                    // shrinkWrap: true,
+                    // physics: NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4, // number of items in each row
+                      mainAxisSpacing: 8.0, // spacing between rows
+                      crossAxisSpacing: 9.0, // spacing between columns
+                    ),
+                    padding:
+                        const EdgeInsets.all(8.0), // padding around the grid
+                    itemCount: 4, // total number of items
+                    itemBuilder: (context, index) {
+                      return OfferCards(
+                        index: index,
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: height(context, 0.05),
+                ),
+              ],
+            ),
           ),
           Positioned(
             bottom: 10,
